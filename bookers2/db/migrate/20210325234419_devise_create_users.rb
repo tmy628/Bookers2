c86c511c4ae352fd870f54466e9f19f36d492e36
tmy628 <tmy6h1r@gmail.com>
2021-04-06 05:58:03 +0000

    [Remove] 不要なマグレーションファイルを削除

--- bookers2/db/migrate/20210325234419_devise_create_users.rb
+++ bookers2/db/migrate/20210325234419_devise_create_users.rb
@@ -33,7 +33,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
       t.string :name
-      
+      t.string :introduction
+      t.string :profile_image_id
 
       t.timestamps null: false
     end
--- bookers2/db/schema.rb
+++ bookers2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_024647) do
+ActiveRecord::Schema.define(version: 2021_03_28_012229) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
@@ -28,10 +28,10 @@ ActiveRecord::Schema.define(version: 2021_03_28_024647) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "introduction"
     t.string "profile_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
